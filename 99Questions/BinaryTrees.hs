8253ee683d2052e8fc223ef6f6dab4d79da3d932
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-03-20 23:19:02 +0100

    Answer problem 55 of the 99 problems.

---
+++ 99Questions/BinaryTrees.hs
@@ -0,0 +1,23 @@
+-- Problems 55 to 60
+
+module BinaryTrees (Tree) where
+
+data Tree a = Empty | Branch a (Tree a) (Tree a)
+              deriving (Show, Eq)
+
+
+-- Problem 55 : Construct completely balanced binary trees.
+
+complBalTree :: Int -> [Tree Char]
+complBalTree 0 = [Empty]
+complBalTree 1 = [Branch 'x' Empty Empty]
+complBalTree n =
+  concat $
+  map (\(nbl,nbr) -> [Branch 'x' l r | l <- (complBalTree nbl), r <- (complBalTree nbr)]) $
+  part2 n
+
+part2 :: Int -> [(Int,Int)]
+part2 n
+  | n == 0 = [(0,0)]
+  | even n = let n2 = n `div` 2 in [(n2,n2)]
+  | odd n = let n2 = n `div` 2 in [(n2, n2+1),(n2+1,n2)]
