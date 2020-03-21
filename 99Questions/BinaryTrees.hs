31af2a5a18e73123f7386b8a1dcd691c7166acf0
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-03-21 16:40:54 +0100

    Answer problem 56.

--- 99Questions/BinaryTrees.hs
+++ 99Questions/BinaryTrees.hs
@@ -19,3 +19,15 @@ part2 n
   | n == 0 = [(0,0)]
   | even n = let n2 = n `div` 2 in [(n2,n2)]
   | odd n = let n2 = n `div` 2 in [(n2, n2+1),(n2+1,n2)]
+
+-- Problem 56 : Check that a tree is symmetric
+
+isSym :: Tree a -> Bool
+isSym Empty = True
+isSym (Branch _ l r) = mirror l r
+
+mirror :: Tree a -> Tree a -> Bool
+mirror Empty Empty = True
+mirror Empty _ = False
+mirror _ Empty = False
+mirror (Branch _ l1 r1) (Branch _ l2 r2) = (mirror l1 r2) && (mirror r1 l2)
