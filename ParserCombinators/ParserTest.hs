b50c7175deeccd57428c41959a4438ea0e563083
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-03-30 22:20:07 +0200

    Clean the Parsers.

--- ParserCombinators/ParserTest.hs
+++ ParserCombinators/ParserTest.hs
@@ -9,29 +9,25 @@ data DoubleVal = TwoNum (Int,Int) | AlphaNum (Char,Int) deriving (Show)
 
 parserDoubleVal = twoNums <|> alphaAndNum
 
+twoNums :: Parser DoubleVal
+twoNums =
+  (,) <$> digitParse <* (char '-') <*> digitParse
+  >>= (\x -> return (TwoNum x))
+
+alphaAndNum :: Parser DoubleVal
+alphaAndNum =
+  (,) <$> letter <* char '-' <*> digitParse
+  >>= (\(x,y) -> return (AlphaNum (x,y)))
+
 digitParse :: Parser Int
 digitParse = digitToInt <$> digit
 
-pairNums :: Parser (Int,Int)
-pairNums = (,) <$> (digitParse <* (char '-')) <*> digitParse
-
-twoNums :: Parser DoubleVal
-twoNums = pairNums >>= (\x -> return (TwoNum x))
-
-alphaAndNum :: Parser DoubleVal
-alphaAndNum = do
-             x <- letter
-             _ <- char '-'
-             y <- digit
-             return (AlphaNum (x, digitToInt y))
-
 data Plate = Plate Int Int Int Int Char deriving (Show)
+
+plate :: Parser Plate
+plate =
+  (,) <$> count 4 plateNum  <*> letter
+  >>= (\([x1,x2,x3,x4],c) -> return (Plate (read x1) (read x2) (read x3) (read x4) c))
 
 plateNum :: Parser String
 plateNum = (manyTill digit (char '-'))
-
-plate :: Parser Plate
-plate = do
-        [x1,x2,x3,x4] <- count 4 plateNum
-        c <- letter
-        return (Plate (read x1) (read x2) (read x3) (read x4) c)
